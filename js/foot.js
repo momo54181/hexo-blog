6e11b7a2d1d09b8ad5753b5ec887c46018e53535
momo54181 <momo5418@outlook.com>
2023-01-02 17:51:49 +0800

    2023.1.2

--- js/foot.js
+++ js/foot.js
@@ -1,6 +1,6 @@
 // 动态心跳，更改自己的名称
 $(document).ready(function(e){
-    $('.copyright').html('©2022 <i class="fa-fw fas fa-heartbeat card-announcement-animation cc_pointer"></i> By 莫莫');
+    $('.copyright').html('<font style="color:#afb4db">©2022</font> <i class="fa-fw fas fa-heartbeat card-announcement-animation cc_pointer"></i> <font style="color:#afb4db">By 莫莫</font></span> ');
 })
 
 $(document).ready(function(e){
@@ -9,7 +9,7 @@ $(document).ready(function(e){
 
 //本站运行时间，更改自己建立站点的时间
 function show_date_time(){
-$('.framework-info').html('小破站已经安全运行<span id="span_dt_dt" style="color: #fff;"></span>');
+$('.framework-info').html('<span id="span_dt_dt" style="color: #afb4db;"></span>');
 window.setTimeout("show_date_time()", 1000);
 BirthDay=new Date("4/30/2021 0:0:0");
 today=new Date();
@@ -24,5 +24,5 @@ hrsold=Math.floor(e_hrsold);
 e_minsold=(e_hrsold-hrsold)*60;
 minsold=Math.floor((e_hrsold-hrsold)*60);
 seconds=Math.floor((e_minsold-minsold)*60);
-span_dt_dt.innerHTML='<font style=color:#afb4db>'+daysold+'</font> 天 <font style=color:#f391a9>'+hrsold+'</font> 时 <font style=color:#fdb933>'+minsold+'</font> 分 <font style=color:#a3cf62>'+seconds+'</font> 秒';
+span_dt_dt.innerHTML='<font style=color:#afb4db> 小破站已经安全运行 </font><font style=color:#afb4db>'+daysold+'</font> 天 <font style=color:#f391a9>'+hrsold+'</font> 时 <font style=color:#fdb933>'+minsold+'</font> 分 <font style=color:#a3cf62>'+seconds+'</font> 秒';
 }
